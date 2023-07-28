107c733bff9757a92a491bc3ccfd9a958ab521a0
MEnisSen <55381972+MEnisSen@users.noreply.github.com>
2023-07-28 15:54:07 +0300

    Add files via upload

--- rl_server/rl_server_no_training.py
+++ rl_server/rl_server_no_training.py
@@ -37,6 +37,8 @@ SUMMARY_DIR = './results'
 LOG_FILE = './results/log'
 # in format of time_stamp bit_rate buffer_size rebuffer_time video_chunk_size download_time reward
 # NN_MODEL = None
+
+#NN_MODEL = '../rl_server/results/nn_model_ep_83800.ckpt'
 NN_MODEL = '../rl_server/results/pretrain_linear_reward.ckpt'
 
 # video chunk sizes
--- run_exp/run_video(yedek).py
+++ run_exp/run_video(yedek).py
@@ -21,65 +21,75 @@ from time import sleep
 # the default location for firefox is /usr/bin/firefox and chrome binary is /usr/bin/google-chrome
 # if they are at those locations, don't need to specify
 
+
 def timeout_handler(signum, frame):
 	raise Exception("Timeout")
 
-run_time = int(sys.argv[1])
-process_id = sys.argv[2]
-sleep_time = sys.argv[3]
-
+ip = sys.argv[1]
+abr_algo = sys.argv[2]
+run_time = int(sys.argv[3])
+process_id = sys.argv[4]
+trace_file = sys.argv[5]
+sleep_time = sys.argv[6]
+	
 # prevent multiple process from being synchronized
 sleep(int(sleep_time))
-
-# generate url  (Akshay: use mahimahi base)
-url = 'http://{}/'.format(os.environ["MAHIMAHI_BASE"]) + 'myindex_RL.html'
+	
+# generate url
+url = 'http://' + ip + '/' + 'myindex_' + abr_algo + '.html'
 
 # timeout signal
 signal.signal(signal.SIGALRM, timeout_handler)
 signal.alarm(run_time + 30)
-
+	
 try:
 	# copy over the chrome user dir
-	abr_path = os.path.dirname(os.path.realpath(__file__))
-	default_chrome_user_dir = abr_path + '/abr_browser_dir/chrome_data_dir'
+	default_chrome_user_dir = '../abr_browser_dir/chrome_data_dir'
 	chrome_user_dir = '/tmp/chrome_user_dir_id_' + process_id
 	os.system('rm -r ' + chrome_user_dir)
 	os.system('cp -r ' + default_chrome_user_dir + ' ' + chrome_user_dir)
-
-	# set up proxy
-	run_proxy = subprocess.Popen(os.path.join(abr_path, 'local-unix-proxy/target/release/unixskproxy -p 8333 -u /tmp/abr_http_socket'),
-		shell=True)
+	
+	# start abr algorithm server
+	if abr_algo == 'RL':
+		command = 'exec /usr/bin/python ../rl_server/rl_server_no_training.py ' + trace_file
+	elif abr_algo == 'fastMPC':
+		command = 'exec /usr/bin/python ../rl_server/mpc_server.py ' + trace_file
+	elif abr_algo == 'robustMPC':
+		command = 'exec /usr/bin/python ../rl_server/robust_mpc_server.py ' + trace_file
+	else:
+		command = 'exec /usr/bin/python ../rl_server/simple_server.py ' + abr_algo + ' ' + trace_file
+	
+	proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	sleep(2)
-
+	
 	# to not display the page in browser
 	display = Display(visible=0, size=(800,600))
 	display.start()
-
+	
 	# initialize chrome driver
 	options=Options()
-	chrome_driver = abr_path + '/abr_browser_dir/chromedriver'
+	chrome_driver = '../abr_browser_dir/chromedriver'
 	options.add_argument('--user-data-dir=' + chrome_user_dir)
 	options.add_argument('--ignore-certificate-errors')
-	options.add_argument('--autoplay-policy=no-user-gesture-required')
 	driver=webdriver.Chrome(chrome_driver, chrome_options=options)
-
+	
 	# run chrome
 	driver.set_page_load_timeout(10)
 	driver.get(url)
-
+	
 	sleep(run_time)
-
+	
 	driver.quit()
 	display.stop()
-
+	
 	# kill abr algorithm server
-	run_proxy.send_signal(signal.SIGINT)
-	# run_proxy.kill()
-
+	proc.send_signal(signal.SIGINT)
+	# proc.kill()
+	
 	print('done')
-
+	
 except Exception as e:
-	try:
+	try: 
 		display.stop()
 	except:
 		pass
@@ -91,6 +101,6 @@ except Exception as e:
 		proc.send_signal(signal.SIGINT)
 	except:
 		pass
-
-	print(e)
+	
+	print(e	)
 
